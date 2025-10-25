11baef56b15d2bc9951dd23ef7470a6d498a598d
PehansaPiyamini1 <as20240469@sci.sjp.ac.lk>
2025-10-25 21:20:08 +0530

    create a calculate part in calculation function to find Total Cost

--- Final/main.c
+++ Final/main.c
@@ -6,7 +6,7 @@
 char city[NUMBER_OF_CITIES];
 int distance[NUMBER_OF_CITIES][NUMBER_OF_CITIES],vehicle[][5];
 int a,b,c,d,e,f,g,h,S,E,D,i,R,time,fuelUsed,fuelPrice;
-float weight,F,W,cost,FUEL_COST;
+float weight,F,W,cost,FUEL_COST,totalCost;
 
 void cityManagement(char city[NUMBER_OF_CITIES]);
 void distanceManagement(char city[NUMBER_OF_CITIES],int distance[NUMBER_OF_CITIES][NUMBER_OF_CITIES]);
@@ -147,6 +147,9 @@ void calculation(int S,int E,float F,int distance[NUMBER_OF_CITIES][NUMBER_OF_CI
 
     FUEL_COST = fuelUsed*F;
     printf("d.Fuel Cost       = %.2f LKR\n\n",FUEL_COST);
+
+    totalCost = cost + FUEL_COST;
+    printf("e.Total Cost      = %.2f LKR\n\n",totalCost);
 
 
 
